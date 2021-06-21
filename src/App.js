f2bba13c70b2cab7c401a851cde9816d3712d0a4
Liza23 <lizadahiya01@gmail.com>
2021-06-21 12:03:08 +0530

    process-env-added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Blogs from "./components/Blog/Blogs";
-import TwoYearsAtIITB from "./components/Blog/TwoYearsAtIITB"
+import TwoYearsAtIITB from "./components/Blog/TwoYearsAtIITB";
 import Experience from "./components/Experience/Experience";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
@@ -30,12 +30,21 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/project" component={Projects} />
-          <Route path="/about" component={About} />
-          <Route path="/experience" component={Experience} />
-          <Route path="/blogs" component={Blogs} />
-          <Route path="/my-2-years-at-IITB" component={TwoYearsAtIITB}/>
+          <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
+          <Route
+            path={process.env.PUBLIC_URL + "/project"}
+            component={Projects}
+          />
+          <Route path={process.env.PUBLIC_URL + "/about"} component={About} />
+          <Route
+            path={process.env.PUBLIC_URL + "/experience"}
+            component={Experience}
+          />
+          <Route path={process.env.PUBLIC_URL + "/blogs"} component={Blogs} />
+          <Route
+            path={process.env.PUBLIC_URL + "/my-2-years-at-IITB"}
+            component={TwoYearsAtIITB}
+          />
         </Switch>
         <Footer />
       </div>
